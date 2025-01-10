dee9b1a0f96b81f23b6dc23ac25e646b6466fb1d
gibranar <gibranabyan082@gmail.com>
2025-01-10 13:45:26 +0700

    feat: back to top button and social media icon hover

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,6 +28,16 @@
         @include('partials.footer')
     </footer>
 
+    <span id="scrollToTopButton"
+        class="fixed bottom-4 right-4 bg-primaryDark p-2 rounded-full opacity-0 translate-y-4 transition-all animate-bounce duration-500 ease-in-out">
+        <a href="#home" class="text-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+            </svg>
+        </a>
+    </span>
+
     @yield('scripts')
     <script>
         document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -41,6 +51,18 @@
                     });
                 }
             });
+        });
+
+        const scrollToTopButton = document.getElementById('scrollToTopButton');
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                scrollToTopButton.classList.remove('opacity-0', 'translate-y-4');
+                scrollToTopButton.classList.add('opacity-100', 'translate-y-0');
+            } else {
+                scrollToTopButton.classList.remove('opacity-100', 'translate-y-0');
+                scrollToTopButton.classList.add('opacity-0', 'translate-y-4');
+            }
         });
     </script>
 </body>
